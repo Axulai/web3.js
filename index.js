88ff2d1c278f4e173e120872f580603fca656d3b
阿来 <yinxulai@hotmail.com>
2018-07-25 12:02:50 +0800

    init

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var Web3 = require('./lib/web3');
 
+// wechat app
+if (typeof wx !== 'undefined' && typeof wx.Web3 === 'undefined') {
+    wx.Web3 = Web3;
+}
+
 // dont override global variable
 if (typeof window !== 'undefined' && typeof window.Web3 === 'undefined') {
     window.Web3 = Web3;
